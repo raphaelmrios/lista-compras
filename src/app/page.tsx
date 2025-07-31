bd46406d4d2e3d7009d87de073d82f424f2e1476
Raphael Rios <raphael.mr@hotmail.com>
2025-07-31 15:22:01 -0300

    APP EM DEV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Lista de Compras Bonitinha 😎
+        Lista de Compras Bonitinha 😎 - APP EM DESENVOLVIMENTO
       </h1>
       <p className="mt-4 text-lg text-gray-700">Feita com Next.js + Tailwind</p>
     </main>
